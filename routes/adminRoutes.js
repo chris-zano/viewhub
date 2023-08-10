93e50f4dad5938df035c48b6f10cc57ff20b5616
Christian Solomon <cncs.chris@gmail.com>
2023-08-10 22:12:40 +0000

    this is important code

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const adminController = require('../controllers/adminController');
-router.get("/", (req, res) => {
-    console.log("It works motherfuckers");
-    res.render("index");
+
+router.get('/', (req,res) => {
+    console.log("This is working");
+    res.render("index")
 })
+
+router.get('/login', (req, res) => {
+    // console.log(req);
+    console.log("================== ",req.headers['user-agent']);
+    console.log("================== ",req.headers['user-agent']);
+    res.status(200  ).json({message: "Login request received successfully"});
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-//This line should initialise the server
-
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
 
-app.use(express.urlencoded({extended : true}));
-app.use('public',express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const adminRoutes = require('./routes/adminRoutes');
 
-const fileRoutes = require('./routes/fileRoutes');
-const adminRoutes = require('./routes/adminRoutes')
+app.use(express.urlencoded({extended: true}));
+app.use("public", express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(adminRoutes);
 
 
-const port = process.env.PORT || 5500
-app.listen(port, ()=>console.log(`Listening on port ${port}`));
+
+const port = process.env.port || 5500
+app.listen(5500, ()=> {
+    console.log(`listening on port ${port}`);
+})
